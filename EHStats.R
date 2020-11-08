057755e144ae4b468e308cdc3e3054fab0a37bf5
Eric Hirsch <ericonsi@yahoo.com>
2020-11-08 16:48:54 -0500

    x

---
+++ EHStats.R
@@ -0,0 +1,8 @@
+
+EH_RemoveOutliersBasedOnQuartile <- function(dframe, col)
+{
+  Q <- quantile(col, probs=c(.25, .75), na.rm = FALSE)
+  iqr <- IQR(col)
+  NewDataFrame<- subset(dframe, col> (Q[1] - 1.5*iqr) & col < (Q[2]+1.5*iqr))
+  return(NewDataFrame)
+}
