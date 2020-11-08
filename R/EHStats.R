30766038fec93dba493a0fd47cf1f63c2efcfbd5
Eric Hirsch <ericonsi@yahoo.com>
2020-11-08 18:05:49 -0500

    x

--- R/EHStats.R
+++ R/EHStats.R
@@ -4,6 +4,6 @@ EH_RemoveOutliersBasedOnQuartile <- function(dframe, col)
   Q <- quantile(col, probs=c(.25, .75), na.rm = FALSE)
   iqr <- IQR(col)
   NewDataFrame<- subset(dframe, col> (Q[1] - 1.5*iqr) & col < (Q[2]+1.5*iqr))
+  print("Outliers Removed.")
   return(NewDataFrame)
-  print("Ouliers Removed.")
 }
